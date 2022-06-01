68145d4e01c2adf5bdcf182c31d03c89e3dfc1ec
Jiayuan <forrestchang7@gmail.com>
2022-06-02 00:54:04 +0800

    Update

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("745d03d647c4b118f671c49214420639cb3af7152e81f132478ed1c649d4597d" default))
+   '("d6844d1e698d76ef048a53cefe713dbbe3af43a1362de81cdd3aefa3711eae0d" "745d03d647c4b118f671c49214420639cb3af7152e81f132478ed1c649d4597d" default))
  '(package-selected-packages
    '(typescript-mode go-mode lsp-treemacs lsp-ivy lsp-ui lsp-mode format-all git-gutter-fringe ace-window evil-collection magit hl-todo rainbow-mode projectile writeroom-mode visual-fill-column doom-themes mixed-pitch doom-modeline super-save undo-fu simpleclip evil-anzu smartparens company-prescient ivy-prescient prescient hydra general evil-snipe counsel all-the-icons-ivy-rich ivy company evil-surround evil which-key use-package)))
 (custom-set-faces
